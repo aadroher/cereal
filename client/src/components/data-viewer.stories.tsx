e413c722e0602ac77c285ebcf7d65d8eadfb0602
Armand Adroher <aadroher@gmail.com>
2022-11-17 22:09:15 +0100

    Adds fake data generation for Storybook story.

--- client/src/components/data-viewer.stories.tsx
+++ client/src/components/data-viewer.stories.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { DateTime } from "luxon";
+import { faker } from "@faker-js/faker";
 
-import DataViewer from './data-viewer';
+import DataViewer, { DataPoint } from "./data-viewer";
+
+const mockLabels = ["A", "B", "C"];
+
+const numSamples = 50;
+
+const intialDate = DateTime.utc();
+const binSizeMin = 5;
+const binRange = [...Array(numSamples).keys()];
+const minVal = 0;
+const maxVal = 100;
+
+const data: DataPoint[] = mockLabels.flatMap((label) =>
+  binRange.map((i) => ({
+    timestamp: intialDate.plus({ minutes: i * binSizeMin }).toJSDate(),
+    name: label,
+    value: faker.datatype.number({ min: minVal, max: maxVal }),
+  }))
+);
 
 export default {
-  title: 'Components/DataViewer',
+  title: "Components/DataViewer",
   component: DataViewer,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 } as ComponentMeta<typeof DataViewer>;
 
-const Template: ComponentStory<typeof DataViewer> = (args) => <DataViewer {...args} />;
+const Template: ComponentStory<typeof DataViewer> = (args) => {
+  console.log(data);
+  return <DataViewer {...args} />;
+};
 
 export const Default = Template.bind({});
 Default.args = {};
--- client/src/components/data-viewer.tsx
+++ client/src/components/data-viewer.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-type DataViewerProps = {}
+export type DataPoint =  {
+  timestamp: Date,
+  name: string,
+  value: number
+}
+
+type DataViewerProps = {
+  data: DataPoint[]
+}
 
 const DataViewer = (props: DataViewerProps): JSX.Element => {
   return <div>The data viewer</div>;
