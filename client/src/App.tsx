1f9ebc8e4ad1d673656228f7a9b7ac7725ace6f4
Armand Adroher <aadroher@gmail.com>
2022-11-17 01:09:54 +0100

    Gets to fetch data from the back end.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const fetchFromApi = async () => {
+     const response = await fetch('/api/v0/metrics')
+     console.log(response)
+     const responseBody = await response.json()
+     console.log(responseBody)
+    }
+    fetchFromApi()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
