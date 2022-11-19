6871c4a3c260cf8068e7434e02d637ae48a36ce4
Armand Adroher <aadroher@gmail.com>
2022-11-19 20:02:11 +0100

    Makes the chart responsive.

--- client/src/components/chart.tsx
+++ client/src/components/chart.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  ResponsiveContainer,
   LineChart,
   Line,
   CartesianGrid,
@@ -38,21 +39,25 @@ const Chart = ({ data, filters }: ChartProps): JSX.Element => {
   const dataNames = getDataNames(data);
 
   return (
-    <LineChart width={1200} height={400} data={dataForGraph}>
-      <CartesianGrid strokeDasharray="3 3" />
-      {filters.names.map((name, i) => (
-        <Line
-          key={name}
-          // type="monotone"
-          dataKey={name}
-          stroke={colourPool[dataNames.indexOf(name)]}
-        />
-      ))}
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-    </LineChart>
+    <ResponsiveContainer minWidth={800} minHeight={200}>
+      <LineChart data={dataForGraph}>
+        <CartesianGrid strokeDasharray="3 3" />
+        {filters.names.map((name, i) => (
+          <Line
+            key={name}
+            type="monotone"
+            dot={false}
+            dataKey={name}
+            isAnimationActive={false}
+            stroke={colourPool[dataNames.indexOf(name)]}
+          />
+        ))}
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
