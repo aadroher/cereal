01bb30a5b9e0931f686943b4dc7e9ebf6b4f8bc3
Armand Adroher <aadroher@gmail.com>
2022-11-20 13:28:37 +0100

    Adds some basic render tests.

---
+++ client/src/containers/data-viewer-mock-responses.tsx
@@ -0,0 +1,138 @@
+export const mockResponseBody = [
+  {
+    timestamp: "2022-11-17T11:00:00.000+00:00",
+    values: {
+      insolation: 48.08526067148445,
+      pressure: 53.005938856465995,
+      temperature: 45.94771080222377,
+    },
+  },
+  {
+    timestamp: "2022-11-17T12:00:00.000+00:00",
+    values: {
+      insolation: 51.94678098068639,
+      pressure: 47.760624585861024,
+      temperature: 47.921712233972954,
+    },
+  },
+  {
+    timestamp: "2022-11-17T13:00:00.000+00:00",
+    values: {
+      insolation: 39.689545670813374,
+      pressure: 51.2829299788265,
+      temperature: 44.52351625203029,
+    },
+  },
+  {
+    timestamp: "2022-11-17T14:00:00.000+00:00",
+    values: {
+      insolation: 44.10827517404513,
+      pressure: 61.95143477281117,
+      temperature: 50.0343279389014,
+    },
+  },
+  {
+    timestamp: "2022-11-17T15:00:00.000+00:00",
+    values: {
+      insolation: 46.820609744536384,
+      pressure: 44.84082472680334,
+      temperature: 47.470223108725236,
+    },
+  },
+  {
+    timestamp: "2022-11-17T16:00:00.000+00:00",
+    values: {
+      insolation: 47.789559834306814,
+      pressure: 48.4698380898656,
+      temperature: 45.748657759427026,
+    },
+  },
+  {
+    timestamp: "2022-11-17T17:00:00.000+00:00",
+    values: {
+      insolation: 45.34117898423678,
+      pressure: 51.69192535931679,
+      temperature: 59.497064144589984,
+    },
+  },
+  {
+    timestamp: "2022-11-17T18:00:00.000+00:00",
+    values: {
+      insolation: 52.535495063693396,
+      pressure: 50.7442929003795,
+      temperature: 47.768973749257704,
+    },
+  },
+  {
+    timestamp: "2022-11-17T19:00:00.000+00:00",
+    values: {
+      insolation: 58.065354517861934,
+      pressure: 53.168003716979705,
+      temperature: 47.42439167762779,
+    },
+  },
+  {
+    timestamp: "2022-11-17T20:00:00.000+00:00",
+    values: {
+      insolation: 61.24632414626891,
+      pressure: 58.55035497219781,
+      temperature: 54.35644670900472,
+    },
+  },
+  {
+    timestamp: "2022-11-17T21:00:00.000+00:00",
+    values: {
+      insolation: 39.2832851722058,
+      pressure: 53.878305740662036,
+      temperature: 63.12764085152947,
+    },
+  },
+  {
+    timestamp: "2022-11-17T22:00:00.000+00:00",
+    values: {
+      insolation: 36.74686882029062,
+      pressure: 56.75095659719659,
+      temperature: 52.645926737506926,
+    },
+  },
+  {
+    timestamp: "2022-11-17T23:00:00.000+00:00",
+    values: {
+      insolation: 48.281469330191456,
+      pressure: 59.78505157528448,
+      temperature: 45.76602840535017,
+    },
+  },
+  {
+    timestamp: "2022-11-18T00:00:00.000+00:00",
+    values: {
+      insolation: 46.835170248837,
+      pressure: 49.268872149476934,
+      temperature: 52.66070671553254,
+    },
+  },
+  {
+    timestamp: "2022-11-18T01:00:00.000+00:00",
+    values: {
+      insolation: 41.216625288858744,
+      pressure: 49.702773743115785,
+      temperature: 48.937683579155525,
+    },
+  },
+  {
+    timestamp: "2022-11-18T02:00:00.000+00:00",
+    values: {
+      insolation: 51.54687390779491,
+      pressure: 44.69589897012671,
+      temperature: 56.328371810762405,
+    },
+  },
+  {
+    timestamp: "2022-11-18T03:00:00.000+00:00",
+    values: {
+      insolation: 41.70303658892714,
+      pressure: 54.363410705015276,
+      temperature: 45.60691578350905,
+    },
+  },
+];
