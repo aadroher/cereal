b61cbb2cd15b29642cfacf3f576a287b61ac0620
Armand Adroher <aadroher@gmail.com>
2022-11-20 03:04:48 +0100

    WIP

--- client/src/containers/data-viewer-container.spec.tsx
+++ client/src/containers/data-viewer-container.spec.tsx
@@ -1,20 +1,30 @@
-import DataViewerContainer from "./data-viewer-container";
-import { render, screen } from "@testing-library/react";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 
-jest.mock("recharts", () => {
-  const OriginalModule = jest.requireActual("recharts");
-  return {
-    ...OriginalModule,
-    ResponsiveContainer: ({ children }) => (
-      <OriginalModule.ResponsiveContainer width={800} height={800}>
-        {children}
-      </OriginalModule.ResponsiveContainer>
-    ),
-  };
-});
+import React from "react";
+import { act } from "react-dom/test-utils";
+import { render, screen } from "@testing-library/react";
+import ResizeObserverPolyfill from "resize-observer-polyfill";
+import "jest-fetch-mock";
+
+import DataViewerContainer from "./data-viewer-container";
+
+window.ResizeObserver = ResizeObserverPolyfill;
 
 describe("DataViewerContainer", () => {
-  it("renders!", () => {
-    render(<DataViewerContainer />);
+  it("renders!", async () => {
+    fetch.mockResponse(async (request) => {
+      return {
+        body: JSON.stringify([
+          {
+            timestamp: new Date().toISOString(),
+            name: "foo",
+            value: 1981,
+          },
+        ]),
+      };
+    });
+
+    await act(async () => render(<DataViewerContainer />));
   });
 });
